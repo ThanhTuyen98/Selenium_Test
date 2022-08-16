599f5e4062ef3dce219bc0fa0ceb4b87c5b2283b
ThanhTuyen98 <huynhthithantuyencn98@gmail.com>
2022-08-16 23:19:53 +0700

    support

--- supports/login.js
+++ supports/login.js
@@ -4,7 +4,7 @@ import { baseUrl, authEmail, authPassword } from "../utils/config";
 export const doLogin = async (driver, { email, password }) => {
   await driver.get(baseUrl);
   await driver.findElement(By.css('.navbar .login')).click();
-  await driver.findElement(By.css('#login-email')).sendKeys(email);
+  await driver.findElement(By.id('login-email')).sendKeys(email);
   await driver.findElement(By.css('#login-password')).sendKeys(password);
   await driver.findElement(By.css('#login-form .approve')).click();
   await driver.sleep(2000);
